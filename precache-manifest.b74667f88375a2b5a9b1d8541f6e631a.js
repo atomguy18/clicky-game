b9b09ecfd1cad35541131d58daccf0a83c996829
atomguy <scrostkowski@gmail.com>
2020-01-02 12:57:14 -0500

    Updates

---
+++ precache-manifest.b74667f88375a2b5a9b1d8541f6e631a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "334700078181f773badf63d62732cca4",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "677e8c87760c688dc2e9",
+    "url": "/clicky-game/static/css/main.28aa010f.chunk.css"
+  },
+  {
+    "revision": "7af7b0f2ab60a10fb603",
+    "url": "/clicky-game/static/js/2.065dacff.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/clicky-game/static/js/2.065dacff.chunk.js.LICENSE"
+  },
+  {
+    "revision": "677e8c87760c688dc2e9",
+    "url": "/clicky-game/static/js/main.50cc4474.chunk.js"
+  },
+  {
+    "revision": "b2fbd57c201176f71099",
+    "url": "/clicky-game/static/js/runtime-main.84ebdde9.js"
+  }
+]);
